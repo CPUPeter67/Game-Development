c4ea2eddc0f3ce91b0987c53612e5de029227834
CPUPeter67 <c.nwodo@abujaprep.com>
2026-09-14 19:02:17 +0100

    Refactor tkinter_demo.py for improved structure and widget association

--- tkinter_demo.py
+++ tkinter_demo.py
@@ -2,18 +2,14 @@ from datetime import date
 import tkinter as tk
 
 
-window = tk.Tk()
-window.title("Demo Window")
-window.geometry("400x300")
-window.mainloop()
 root = tk.Tk()
 root.title("Getting Started with Widgets")
 root.geometry("400x300")
 
-lbl = tk.Label(text="Hello, Tkinterer!", fg="white", bg="#072FSF", height=1, width=300)
+lbl = tk.Label(root, text="Hello, Tkinterer!", fg="white", bg="#072F5F", height=1, width=300)
 
-name_lbl = tk.Label(text="Full Name", bg="#3895D3")
-name_entry = tk.Entry()
+name_lbl = tk.Label(root, text="Full Name", bg="#3895D3")
+name_entry = tk.Entry(root)
 text_box = tk.Text(root, height=5, width=40)
 
 def display():
@@ -25,9 +21,7 @@ def display():
     text_box.insert(tk.END, message)
     text_box.insert(tk.END, date.today())
 
-text_box = tk.Text(height=3)
-
-btn = tk.Button(text="Begin", command=display, height=1, bg="#1261A0", fg='white')
+btn = tk.Button(root, text="Begin", command=display, height=1, bg="#1261A0", fg='white')
 
 lbl.pack()
 name_lbl.pack()
